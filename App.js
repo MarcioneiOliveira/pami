209fbd354f6150b9c0f97c40111d28ba7bde4178
MarcioneiOliveira <124743236+MarcioneiOliveira@users.noreply.github.com>
2023-05-02 21:31:28 -0300

    inicializacao

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import index from './components/apostila_03/index';
+
 export default function App() {
   return (
     <View style={styles.container}>
