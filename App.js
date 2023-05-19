2048759a70d3ecd08c818beeae3e1dc2fb45c2ff
MarcioneiOliveira <124743236+MarcioneiOliveira@users.noreply.github.com>
2023-05-18 21:18:32 -0300

    aula 18052023

--- App.js
+++ App.js
@@ -2,11 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native'; 
 
 import Apostila03A from './components/apostila_03_avancado/';
+import Apostila04 from './components/apostila_04/';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Apostila03A />
+      <Apostila04 />
       <StatusBar style="auto" />
     </View>
   );
@@ -16,7 +17,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    //alignItems: 'center',
     justifyContent: 'center',
   },
 });
